3169db2dea654fb81585e73d7a76f492b0f20955
Paul van den Berg <paul.vandenberg@nedap.com>
2015-10-13 20:59:45 +0200

    json rest service

---
+++ src/main/java/com/nedap/workshop/java/webservice/JaxRsApplication.java
@@ -0,0 +1,11 @@
+package com.nedap.workshop.java.webservice;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * @author paul.vandenberg
+ */
+@ApplicationPath("/rest")
+public class JaxRsApplication extends Application {
+}
